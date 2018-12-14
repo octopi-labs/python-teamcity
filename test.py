3c87eb93175369c5c6fcf12e37c5dc5f7f30921a
Rahul Shelke <srahul07@gmail.com>
2018-12-14 18:28:29 +0530

    Adding test

--- test.py
+++ test.py
@@ -1,4 +1,17 @@
+import os
 from teamcity import Teamcity
 
+from teamcity.config import TEAMCITY_BASIC_AUTH, TEAMCITY_GUEST_AUTH
+
+os.environ['TEAMCITY_USERNAME'] = "srahul07"
+os.environ['TEAMCITY_PASSWORD'] = "rahul"
+os.environ['TEAMCITY_SCHEME'] = "http"
+os.environ['TEAMCITY_HOST'] = "localhost"
+os.environ['TEAMCITY_PORT'] = str(8111)
+
+os.environ['TEAMCITY_BASIC_AUTH'] = TEAMCITY_BASIC_AUTH
+os.environ['TEAMCITY_GUEST_AUTH'] = TEAMCITY_GUEST_AUTH
+
 test = Teamcity("srahul07", "rahul", scheme="http", host="localhost", port=8111)
-print(test.session.cookies.get_dict())
+print(test.server_info)
+# print(test.plugins())
